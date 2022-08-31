97b0f3332483afffe6be153ce5ba09daf8e374f8
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-31 08:49:48 +0300

    Add files via upload

---
+++ DownloadPage.java
@@ -0,0 +1,40 @@
+package aSoftUni;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+    class DownloadPage {
+
+        public static void main(String[] args) throws IOException {
+
+            // Make a URL to the web page
+            URL url = new URL("https://uk.finance.yahoo.com/portfolio/p_0/view/v1");
+
+            // Get the input stream through URL Connection
+            URLConnection con = url.openConnection();
+            InputStream is =con.getInputStream();
+
+            // Once you have the Input Stream, it's just plain old Java IO stuff.
+
+            // For this case, since you are interested in getting plain-text web page
+            // I'll use a reader and output the text content to System.out.
+
+            // For binary content, it's better to directly read the bytes from stream and write
+            // to the target file.
+
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            String line = null;
+
+            // read each line and write to System.out
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+}
