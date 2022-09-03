0ec5b19dd8b80aa3b01fcb45b7acd46abe2cc5f5
12z3 <77529497+12z3@users.noreply.github.com>
2022-09-03 14:57:54 +0300

    Add files via upload

---
+++ ImportantTasks.java
@@ -0,0 +1,42 @@
+package aSoftUni;
+
+public class ImportantTasks {
+    // TODO: -> https://www.youtube.com/watch?v=QYO5_riePOQ
+    // TODO: Alt + Enter <-
+    // TODO: 04.05.22 г. ⌃ D - Дупликат на текущия ред на следващият.
+
+    /*
+     Клавишни комбинации: <Alt + Enter> -> Много Широко Обвхватен:
+             ->  "if" = Обръща условието.
+             ->  "" =
+
+     Закръгля -> "long t =  Math.round(0.9);"
+
+     import java.util.Arrays; -> System.out.println(Arrays.toString(c));
+     */
+
+    // TODO: ....
+    //  SearchBinary <-
+    //  MobileOperator <-
+    //  CinemaTicketsNew <-- Добро е.
+    //  Traveling1 <-
+    //  CompGameStore <-
+    //  HightJumpWhile <-
+    //  ChristmasTournament2 <-
+    //  DartsGame1
+    //  ChristmasTournament
+    //  CruiseGames
+    //  CinemaPremiere
+    //  ExamTasks
+    //  FootballGoals
+    //  GamesOfInterval
+    //  HightJumpWhile
+    //  NumbersOddEven
+    //  RepetitiveElements
+    //  CharSequence
+    //  TicketCombination <-
+    //  ASCII_Important
+    //  ASCIIDone <-
+    //  SumOfDigitsOfNumbers <-
+
+}
