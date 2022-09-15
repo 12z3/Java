bfcb2ffc602670a0930f0a21eb116379ebe8bc21
12z3 <77529497+12z3@users.noreply.github.com>
2022-09-15 18:05:46 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,18 @@
+package aSoftUni;
+
+import training.Methods;
+
+public class Test extends Methods {
+
+    public static void main(String[] args) {
+        timeAndData();
+
+        int[] massive = new int[]{0, 3, 10, 0, 0, 3, 5, 3, 7, 0, 0};
+
+        printIntArray(massive);
+        printArrayWithOutZeroElements(massive);
+
+
+
+    }
+}
