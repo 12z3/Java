abd55419d3648c7a08fb5f56e2cb4039ed205fcb
12z3 <77529497+12z3@users.noreply.github.com>
2022-09-20 13:00:32 +0300

    Add files via upload

---
+++ SearhBinary.java
@@ -0,0 +1,32 @@
+package aSoftUni;
+
+import training.Methods;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SearhBinary extends Methods {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        timeAndData();
+
+        int[] arr = { 10, 20, 15, 22, 35 };
+        int key = 22;
+
+        int res = Arrays.binarySearch(arr, key);
+
+        System.out.println(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
